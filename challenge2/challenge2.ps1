747a01134eaa669fb099b467fac2e1e46b7dc32e
steven yeomans <steve@tiggus.com>
2020-06-09 06:59:00 +0100

    updated to function

--- challenge2/challenge2.ps1
+++ challenge2/challenge2.ps1
@@ -1,14 +1,29 @@
-# variables
-$instancdId = 'i-01d2347c1b32ae075'
-$searcher = 'LaunchTime'
-$output = 'json'
-$instanceRegion = 'us-east-2'
+function searcher {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $instanceId,
+         [Parameter(Mandatory=$false, Position=1)]
+         [string] $datakey
+    )
+    # variables
+    $output = 'json'
+    $instanceRegion = 'us-east-2'
 
-# query metadata
-$metadata = aws ec2 describe-instances --instance-id $instancdId --region $instanceRegion --output $output
-
-# get datakey $searcher
-if ($searcher){
-    $lookup = $metadata | ConvertFrom-Json
-    $lookup | Select-Object {$_.Reservations.Instances.$searcher} | select-object -ExpandProperty *
+    # query metadata
+    $metadata = aws ec2 describe-instances --instance-id $instancdId --region $instanceRegion --output $output
+    # get datakey $searcher
+    if ($datakey){
+        $lookup = $metadata | ConvertFrom-Json
+        $lookup | Select-Object {$_.Reservations.Instances.$datakey} | select-object -ExpandProperty *
+    }
+    else {
+        if ($metadata) {$metadata}
+    }
 }
+
+# returns the json metadata
+searcher -instanceId i-123456789
+
+# return the selected data key if exists
+searcher -instanceId i-123456789 -datakey LaunchTimer
