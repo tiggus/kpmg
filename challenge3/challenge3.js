14fcd75232aac5d78e130a2c9c01b68a588c75c3
steven yeomans <steve@tiggus.com>
2020-06-09 19:42:55 +0100

    Danius K - Java

--- challenge3/challenge3.js
+++ challenge3/challenge3.js
@@ -1,7 +1,7 @@
 // tried to do this in powershell with a json nest, no joy so did what I would do at work
 // I asked the experts (programmers) who gave me the base code and talked me through it
 // please see the java.txt for example of the code being run. 30 minutes isnt long !
-// Credit to Danius K !
+// Credit to Danius K. !
 
 var object = {"a1": { "b1": { "c1": "d1" }}};
 
